16dbba14808be09e59704520035a2880ca90a6ae
Savitri <savitri26m@gmail.com>
2020-04-26 15:29:36 +0530

    adding custom range validator

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,17 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+
+// function without using parameters
+// function ratingRange(c: AbstractControl) : {[key:string]: boolean } |null {
+//   if(c.value !== null && (isNaN(c.value) || c.value < 1 || c.value > 5)){
+//     return {range: true};
+//   }
+//   return null;
+// }
+
+
+// modified function by passing parameters, by wrapping it in factory function, which can take more than one parameter
+function ratingRange(min: number, max: number) : ValidatorFn {
+  return (c: AbstractControl): {[key: string]: boolean} | null => {
+    if(c.value !== null && (isNaN(c.value) || c.value < min || c.value > max )){
+      return { 'range': true }
+    }
+    return null;
+  }
+}
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
+
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   // manages instance of customer data that we are binding to in our template
   customer = new Customer();
+
+  // creating custom validator
 
   constructor(private fb: FormBuilder) { }
 
@@ -23,6 +46,7 @@ export class CustomerComponent implements OnInit {
       lastName: [{value: 'Mortey', disabled: false}, [Validators.required, Validators.maxLength(50)]],
       email: ['', [Validators.email, Validators.required]],
       phone: '',
+      rating: [null, ratingRange(1,5)],
       notification: 'email',
       sendCatalog: true
     })
@@ -36,6 +60,7 @@ export class CustomerComponent implements OnInit {
   setNotification(notify: string){
     const phoneControl = this.customerForm.get('phone');
     if(notify === 'text'){
+      //to use multiple validators use array of validators
       phoneControl.setValidators(Validators.required);
     }else{
       phoneControl.clearValidators();
